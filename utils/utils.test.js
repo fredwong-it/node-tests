6c893c357501b4797ca6f24ed61cb41d4811a21b
Fred Wong <fredwong.it@gmail.com>
2017-12-19 15:44:36 -0500

    Using an Assertion Library

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,17 +1,48 @@
+const expect = require('expect');
 const utils = require('./utils');
 
 it('should add two numbers', () => {
     var res = utils.add(33, 11);
 
-    if (res !== 44) {
-        throw new Error(`Expected 44, but got ${res}.`)
-    }
+    expect(res).toBe(44).toBeA('number');
 });
 
 it('should square a number', () => {
     var res = utils.square(3);
 
-    if (res !== 9) {
-        throw new Error(`Expected 9, but got ${res}.`)
-    }
+    expect(res).toBe(9).toBeA('number');
+});
+
+it('should expect some values', () => {
+    //expect(12).toNotBe(11);
+    expect({name: 'Andrew'}).toEqual({name: 'Andrew'});
+    expect({name: 'andrew'}).toNotEqual({name: 'Andrew'});
+    expect([2,3,4]).toInclude(2);
+    expect([2,3,4]).toExclude(5);
+    expect({
+        name: 'Fred',
+        age: 28,
+        location: 'Macau'
+    }).toInclude({
+        age: 28
+    }).toExclude({
+        age: 30
+    });
+});
+
+// should verify first and last names are set
+// asset it includes firstName and last Name with proper values
+// asset the type is object
+it('should set firstName and lastName', () => {
+    var user = {
+        age: 28,
+        location: 'Macau'
+    };
+
+    res = utils.setName(user, 'Fred Wong');
+
+    expect(res).toInclude({
+        firstName: 'Fred',
+        lastName: 'Wong'
+    }).toBeA('object');
 });
