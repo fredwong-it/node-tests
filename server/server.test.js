ca230b8e371aa2a0afdbfb9a1c7c273686d9b72b
Fred Wong <fredwong.it@gmail.com>
2017-12-21 11:50:02 -0500

    Organizing Test With describe()

--- server/server.test.js
+++ server/server.test.js
@@ -3,41 +3,56 @@ const expect = require('expect');
 
 var app = require('./server').app;
 
-it('should return Hello World reponse', (done) => {
-    request(app)
-        .get('/')
-        .expect(404)
-        .expect({
-            error: 'Page not found.',
-            name: 'Todo App v1.0'
-        })
-        .end(done);
+// Server
+    // GET /
+        // some test case
+    // GET / users
+        // some test case
+describe('Server', () => {
+    describe('GET /', () => {
+        it('should return Hello World reponse', (done) => {
+            request(app)
+                .get('/')
+                .expect(404)
+                .expect({
+                    error: 'Page not found.',
+                    name: 'Todo App v1.0'
+                })
+                .end(done);
+        });
+
+        // combination of supertest and expect
+        it('should return Hello World reponse 2', (done) => {
+            request(app)
+                .get('/')
+                .expect((res) => {
+                    expect(res.body).toInclude({
+                        error: 'Page not found.'
+                    });
+                })
+                .end(done);
+        });
+    });
+
+    describe('GET /users', () => {
+        // Make a new test
+        // assert 200
+        // Assert that you exist in users array
+        it('should return my user object', (done) => {
+            request(app)
+                .get('/users')
+                .expect(200)
+                .expect((res) => {
+                    expect(res.body).toInclude({
+                        name: 'Fred',
+                        age: 24
+                    });
+                })
+                .end(done);
+        });
+    });
 });
 
-// combination of supertest and expect
-it('should return Hello World reponse 2', (done) => {
-    request(app)
-        .get('/')
-        .expect((res) => {
-            expect(res.body).toInclude({
-                error: 'Page not found.'
-            });
-        })
-        .end(done);
-});
 
-// Make a new test
-// assert 200
-// Assert that you exist in users array
-it('should return my user object', (done) => {
-    request(app)
-        .get('/users')
-        .expect(200)
-        .expect((res) => {
-            expect(res.body).toInclude({
-                name: 'Fred',
-                age: 24
-            });
-        })
-        .end(done);
-});
+
+
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,33 +1,39 @@
 const expect = require('expect');
 const utils = require('./utils');
 
-it('should add two numbers', () => {
-    var res = utils.add(33, 11);
+describe('Utils', () => {
+    describe('#add', () => {
+        it('should add two numbers', () => {
+            var res = utils.add(33, 11);
+        
+            expect(res).toBe(44).toBeA('number');
+        });
+    });
 
-    expect(res).toBe(44).toBeA('number');
-});
+    // async test case
+    it('should async add two numbers', (done) => {
+        utils.asyncAdd(33, 11, (res) => {
+            expect(res).toBe(44).toBeA('number');
+            done();
+        }); 
+    });
 
-// async test case
-it('should async add two numbers', (done) => {
-    utils.asyncAdd(33, 11, (res) => {
-        expect(res).toBe(44).toBeA('number');
-        done();
-    }); 
-});
-
-it('should square a number', () => {
-    var res = utils.square(3);
-
-    expect(res).toBe(9).toBeA('number');
-});
-
-// async test case
-it('should async square a numbers', (done) => {
-    utils.asyncSquare(3, (res) => {
+    it('should square a number', () => {
+        var res = utils.square(3);
+    
         expect(res).toBe(9).toBeA('number');
-        done();
-    }); 
+    });
+    
+    // async test case
+    it('should async square a numbers', (done) => {
+        utils.asyncSquare(3, (res) => {
+            expect(res).toBe(9).toBeA('number');
+            done();
+        }); 
+    });
 });
+
+
 
 it('should expect some values', () => {
     expect(12).toNotBe(11);
